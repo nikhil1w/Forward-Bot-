45935d71b52b99bba229577a424926a41d2a692f
nikhil1w <rayrambadan70@gmail.com>
2025-08-05 16:49:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "24869695"))
     API_HASH = environ.get("API_HASH", "5ee98927939d175ca953297fbe309f37")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7628985628:AAGqAgRhu6nIJadNKp8oebXiaw1U5oNVfFY") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8286915242:AAE59M_2DrPhpB-4BY0lqgoDyMzFGMRFSRA") 
+    BOT_SESSION = environ.get("BOT_SESSION", "BQF7ez8ACD9IS-DbO8hTVBIBzYMGAIMwgGHj93HsPGvIAjJUqi_rRPIeaXkSfS5vwzykSZqZKBBRnCmhCDxUhVVJi0L34_09f-55tLKgPngqqnJfUZkv65ytHcML-EhfSeJ6kq0_IvPoSx1578_zQ16fl05kMNtyLaJYpblX5jg6R1U9zemUuRXWSdmxr4LcWCyleSI6Vs6AwoMsa5JiBITeLd4xZWbM9Zt0rOg1_P7LyMNaik1lXx3pRbNQWqP0-YrxBNH3ua6bxJk1IquzFOiVb0-DxWUNdqY0WpoyX5S3GiEIlgiyLlUWSpT-KyBeoncWRnTeqrXzA44loxGORH4y_ch19wAAAAHt8EqqAQ") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://editingtution99:kLKimOFEX1MN1v0G@cluster0.fxbujjd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "suraj")
     BOT_OWNER = int(environ.get("BOT_OWNER", "7445620075"))
 
 # Don't Remove Credit Tg - @VJ_Botz
@@ -27,4 +27,5 @@ class temp(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
